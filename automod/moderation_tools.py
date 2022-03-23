5f70d798200c053412a6c41eab1c848919f03dfa
deoncarlette <63321092+deoncarlette@users.noreply.github.com>
2022-03-23 13:05:35 -0500

    Update moderation_tools.py

--- automod/moderation_tools.py
+++ automod/moderation_tools.py
@@ -15,20 +15,7 @@ import boto3
 
 from .clubhouse_api import Clubhouse
 
-active_mod = False
-waiting_mod = False
-active_mod_thread = None
-announce_thread = None
-wait_speaker_thread = None
-listen_ping_thread = None
-welcomed_list_old = []
-_counter = 0
 
-tracking_client = boto3.client('s3')
-
-
-# Load user config settings from AutoMod/mod_config.py
-# Read config.ini
 def read_config(section):
     """
     A function to read the config file.
@@ -40,7 +27,7 @@ def read_config(section):
 
     config_object = config_object[section]
 
-    if section == "Account":
+    if section == "Account" or section == "S3":
         return dict(config_object)
 
     content_list = []
@@ -50,13 +37,31 @@ def read_config(section):
     return content_list
 
 
-mod_list = read_config("ModList")
-guest_list = read_config("GuestList")
-guest_list_2 = read_config("ASocialRoomGuestList")
-guest_list = guest_list + guest_list_2
-approved_ping_list = read_config("ApprovedPing")
-client_id = read_config("Account")
-client_id = client_id['user_id']
+# Load user config settings from AutoMod/mod_config.py
+# Read config.in
+class Var:
+    active_speaker = False
+    waiting_speaker = False
+    active_mod = False
+    waiting_mod = False
+
+    mod_channel_thread = None
+    announce_thread = None
+    wait_speaker_thread = None
+    listen_ping_thread = None
+    music_thread = None
+    welcome_thread = None
+
+    already_welcomed_list = []
+    counter = 0
+
+    mod_list = read_config("ModList")
+    guest_list = read_config("GuestList") + read_config("ASocialRoomGuestList")
+    approved_ping_list = read_config("ApprovedPing")
+
+    client_id = read_config("Account")["user_id"]
+
+    s3_bucket = read_config("S3")["bucket"]
 
 
 def set_interval(interval):
@@ -89,6 +94,16 @@ def set_interval(interval):
     return decorator
 
 
+@set_interval(30)
+def ping_keep_alive_client(client, channel_name):
+    """ (str) -> bool
+
+    Continue to ping alive every 30 seconds.
+    """
+    client.active_ping(channel_name)
+    return True
+
+
 def data_dump(dump, source, channel=""):
 
     dump_loaded = False
@@ -110,6 +125,14 @@ def data_dump(dump, source, channel=""):
         else:
             log = dump
 
+    elif source == 'channel_dict':
+        if dump['channel_info']['success']:
+            data = dump
+            key = f"channel_{dump['channel_info']['channel']}"
+            dump_loaded = True
+        else:
+            log = dump
+
     elif source == 'join':
         if dump['users']:
             data = dump
@@ -127,7 +150,7 @@ def data_dump(dump, source, channel=""):
     if dump_loaded:
         s3_client = boto3.client('s3')
         data = json.dumps(data)
-        bucket = 'iconicbucketch'
+        bucket = Var.s3_bucket
         timestamp = datetime.now(pytz.timezone('UTC')).isoformat()
         key = f"{key}_{timestamp}.json"
 
@@ -138,6 +161,37 @@ def data_dump(dump, source, channel=""):
         )
 
     logging.info(log)
+
+    return
+
+
+def data_dump_client(client, dump=None, source=None, channel=""):
+    is_private = False
+    is_social_mode = False
+
+    if dump:
+        if source == "join":
+            is_private = dump["is_private"]
+            is_social_mode = dump["is_social_mode"]
+
+        elif source == "channel":
+            is_private = dump["is_private"]
+            is_social_mode = dump["is_social_mode"]
+
+        elif source == "channel_dict":
+            is_private = dump["channel_info"]["is_private"]
+            is_social_mode = dump["channel_info"]["is_social_mode"]
+
+        if not is_private and not is_social_mode:
+            data_dump(dump, source, channel)
+
+    else:
+        feed_info = client.get_feed()
+        data_dump(feed_info, 'feed')
+
+        channel_info = client.get_channel(channel)
+        if not channel_info['is_social_mode'] and not channel_info['is_private']:
+            data_dump(channel_info, 'channel', channel)
 
     return
 
@@ -191,25 +245,23 @@ def get_channel_status(client, channel):
     else:
         if not channel_info['success']:
 
-            global active_mod
-            active_mod = False
+            Var.active_mod = False
             logging.info("moderation_tools.get_channel_status Changed active_mod to False")
 
-            global listen_ping_thread
-            listen_ping_thread = listen_channel_ping(client)
+            Var.listen_ping_thread = listen_channel_ping(client)
             logging.info("moderation_tools.get_channel_status Enabled listen_channel_ping")
 
         else:
             i = 0
             user_info = channel_info.pop('users')
-            for _user in user_info:
-                _user_id = _user['user_id']
+            for user in user_info:
+                user_id = user['user_id']
 
-                if str(_user_id) ==  client_id:
+                if str(user_id) == Var.client_id:
 
-                    _client = user_info.pop(i)
+                    client = user_info.pop(i)
 
-                    response_dict['client_info'] = _client
+                    response_dict['client_info'] = client
                     response_dict['channel_info'] = channel_info
                     response_dict['user_info'] = user_info
 
@@ -230,24 +282,24 @@ def wait_speaker_permission(client, channel, user_id):
     # Check if the moderator allowed your request.
     res_inv = client.accept_speaker_invite(channel, user_id)
     if res_inv['success']:
+        Var.active_speaker = True
         return False
 
     return True
 
 
-def request_speaker_permission(client, channel, channel_dict, join_dict, mod=True, music=False):
+def request_speaker_permission(client, channel, channel_dict, join_dict, mod=False, music=False):
 
     creator_id = join_dict['creator_user_profile_id']
     creator_name = ""
-    for _user in join_dict['users']:
-        if creator_id == _user['user_id']:
-            creator_name = _user['first_name']
+    for user in join_dict['users']:
+        if creator_id == user['user_id']:
+            creator_name = user['first_name']
             break
 
     message = "🤖 Hello " + creator_name + "! I'm AutoMod! 🎉"
     send_room_chat(client, channel, message)
-
-    channel_info = channel_dict['channel_info']
+    message = False
 
     client_info = channel_dict['client_info']
     logging.info(f"moderation_tools.request_speaker_permission {client_info}")
@@ -260,19 +312,18 @@ def request_speaker_permission(client, channel, channel_dict, join_dict, mod=Tru
         if mod:
             message = "If you'd like to use my features, please invite me to speak and make me a Moderator."
 
-        elif not mod and music:
+        elif music:
             message = "If you'd like to hear music, please invite me to speak."
 
-        send_room_chat(client, channel, message)
-        global wait_speaker_thread
-        wait_speaker_thread = wait_speaker_permission(client, channel, client_id)
-        logging.info(f"moderation_tools.request_speaker_permission Triggered message {message}")
+        Var.wait_speaker_thread = wait_speaker_permission(client, channel, Var.client_id)
 
-    if client_info['is_speaker'] and not client_info['is_moderator']:
+    elif not client_info['is_moderator']:
         if mod:
-            message = 'Please make me a Moderator.'
-            send_room_chat(client, channel, message)
-            logging.info(f"moderation_tools.request_speaker_permission Triggered message {message}")
+            message = "If you'd like to use my features, please make me a Moderator."
+
+    if message:
+        send_room_chat(client, channel, message)
+        logging.info(f"moderation_tools.request_speaker_permission Triggered message {message}")
 
     return
 
@@ -292,8 +343,7 @@ def welcome_guests(client, channel, user):
 
     send_room_chat(client, channel, message)
 
-    global welcomed_list_old
-    welcomed_list_old.append(user['user_id'])
+    Var.already_welcomed_list.append(user['user_id'])
 
     return
 
@@ -313,85 +363,86 @@ def mod_guests(client, channel, user):
         client.make_moderator(channel, user['user_id'])
         logging.info(f"moderation_tools.mod_guest Made {user['name']} a moderator")
 
-        if user not in welcomed_list_old:
+        if user not in Var.already_welcomed_list:
             welcome_guests(client, channel, user)
 
     return
 
 
-def terminate_mod(client, channel):
-
-    global active_mod
-    active_mod = False
-
-    global waiting_mod
-    waiting_mod = False
-
-    if announce_thread:
-        announce_thread.set()
-
-    if active_mod_thread:
-        active_mod_thread.set()
-
+def termination(client, channel):
     client.leave_channel(channel)
 
-    logging.info("moderation_tools.terminate_mod Active mod terminated")
+    Var.active_speaker = False
+    Var.waiting_speaker = False
+    Var.active_mod = False
+    Var.waiting_mod = False
 
-    global listen_ping_thread
-    listen_ping_thread = listen_channel_ping(client)
+    if Var.mod_channel_thread:
+        Var.mod_channel_thread.set()
+
+    if Var.announce_thread:
+        Var.announce_thread.set()
+
+    if Var.music_thread:
+        Var.music_thread.set()
+
+    if Var.welcome_thread:
+        Var.welcome_thread.set()
+
+    if Var.ping_keep_alive_thread:
+        Var.ping_keep_alive_thread.set()
+
+    Var.listen_ping_thread = listen_channel_ping(client)
+
+    logging.info("moderation_tools.terminate_mod Automation terminated")
 
     return
 
 
 @set_interval(15)
-def mod_channel(client, channel):
-
+def mod_client(client, channel):
     channel_dict = get_channel_status(client, channel)
     channel_info = channel_dict['channel_info']
-
     user_info = channel_dict['user_info']
-    global active_mod
-    global waiting_mod
-    global _counter
 
-    if _counter == 5:
-        _counter = 0
+    if Var.counter == 5:
+        Var.counter = 0
 
+    # Need exception handling
     if not channel_info or not channel_info['success']:
-        terminate_mod(client, channel)
+        termination(client, channel)
         return False
 
     else:
-        logging.info(f"moderation_tools.mod_channel Counter: {_counter}")
         client_speaker_status = channel_dict['client_info']['is_speaker']
         client_mod_status = channel_dict['client_info']['is_moderator']
         social_mode = channel_info['is_social_mode']
         private = channel_info['is_private']
 
-        if active_mod and not client_speaker_status:
-            client.accept_speaker_invite(channel, client_id)
+        if Var.active_mod and not client_speaker_status:
+            client.accept_speaker_invite(channel, Var.client_id)
             logging.info("moderation_tools.mod_channel Client is no longer a speaker")
             logging.info("moderation_tools.mod_channel Client attempted to accept new speaker invitation")
 
             if client_speaker_status:
                 logging.info("moderation_tools.mod_channel Client accepted new speaker invitation")
 
-        elif active_mod and not client_mod_status and not social_mode and _counter == 4:
+        elif Var.active_mod and not client_mod_status and not social_mode and Var.counter == 4:
             logging.info(f"moderation_tools.mod_channel Client is not a moderator")
-            waiting_mod = True
+            Var.waiting_mod = True
 
-        if waiting_mod and not client_mod_status and not social_mode and _counter == 3:
-            terminate_mod(client, channel)
+        if Var.waiting_mod and not client_mod_status and not social_mode and Var.counter == 3:
+            termination(client, channel)
             logging.info("moderation_tools.mod_channel Triggered terminate_mod")
             return False
 
-        active_mod = True
+        Var.active_mod = True
 
         for _user in user_info:
-            _user_id = _user['user_id']
+            user_id = _user['user_id']
 
             if social_mode:
-                if _user_id not in welcomed_list_old:
+                if user_id not in Var.alreay_welcomed_list:
                     welcome_guests(client, channel, _user)
 
             elif private:
@@ -403,68 +454,136 @@ def mod_channel(client, channel):
                 invite_guests(client, channel, _user)
 
             else:
-                if client_mod_status and guest_list:
-                    if str(_user_id) in guest_list:
+                if client_mod_status and Var.guest_list:
+                    if str(user_id) in Var.guest_list:
                         invite_guests(client, channel, _user)
 
-                if client_mod_status and mod_list:
-                    if str(_user_id) in mod_list:
+                if client_mod_status and Var.mod_list:
+                    if str(user_id) in Var.mod_list:
                         mod_guests(client, channel, _user)
 
-
-                # elif channel_info['num_all'] < 30 and channel_info['num_ever'] < 500:
-                #     if _user_id not in welcomed_list_old:
-                #     welcome_guests(client, channel, _user)
-
-        if _counter == 1 or _counter == 3:
-            client.active_ping(channel)
-            logging.info("moderation_tools_v2.mod_channel Triggered clubhouse_api.Clubhouse.active_ping")
-
-        if not channel_info['is_social_mode'] and not channel_info['is_private'] and _counter == 4:
+        if not channel_info['is_social_mode'] and not channel_info['is_private'] and Var.counter == 4:
             feed_info = client.get_feed()
             data_dump(feed_info, 'feed')
-            data_dump(channel_info, 'channel', channel)
+            data_dump(channel_dict, 'channel_dict', channel)
 
-        _counter += 1
+        Var.counter += 1
 
     return True
 
 
-def active_mod_channel(client, channel, announcement=None, interval=3600):
+@set_interval(30)
+def music_client(client, channel):
+    channel_dict = get_channel_status(client, channel)
+    client_info = channel_dict["client_info"]
+    client_speaker_status = client_info["is_speaker"]
 
+    if Var.counter == 7:
+        Var.counter = 0
+
+    if Var.active_speaker and not client_speaker_status:
+        client.accept_speaker_invite(channel, Var.client_id)
+        logging.info("moderation_tools.mod_channel Client is no longer a speaker")
+        logging.info("moderation_tools.mod_channel Client attempted to accept new speaker invitation")
+
+        if client_speaker_status:
+            logging.info("moderation_tools.mod_channel Client accepted new speaker invitation")
+
+    if Var.active_speaker and not client_speaker_status and Var.counter == 4:
+        Var.waiting_speaker = True
+
+    if Var.waiting_speaker and not client_speaker_status and Var.counter == 7:
+        termination(client, channel)
+        logging.info("moderation_tools.mod_channel Triggered terminate_mod")
+        return False
+
+    if Var.counter == 6:
+        feed_info = client.get_feed()
+        data_dump_client(client, feed_info, 'feed')
+        data_dump_client(client, channel_dict, "channel_dict", channel)
+
+    Var.counter += 1
+
+    return
+
+
+@set_interval(180)
+def track_room_client(client, channel):
     join_dict = client.join_channel(channel)
-    client.active_ping(channel)
-
     data_dump(join_dict, 'join', channel)
 
+    return True
+
+
+@set_interval(30)
+def welcome_all_client(client, channel):
     channel_dict = get_channel_status(client, channel)
-    request_speaker_permission(client, channel, channel_dict, join_dict)
+    channel_info = channel_dict["channel_info"]
+    user_info = channel_dict["user_info"]
 
-    global active_mod_thread
-    active_mod_thread = mod_channel(client, channel)
-    # global ping_keep_alive_thread
-    # ping_keep_alive_thread = ping_keep_alive(client, channel)
+    if Var.counter == 7:
+        Var.counter = 0
 
-    global announce_thread
+    for _user in user_info:
+        user_id = _user["user_id"]
+        if user_id not in Var.alreay_welcomed_list:
+            welcome_guests(client, channel, _user)
+
+    if not channel_info or not channel_info["success"]:
+        termination(client, channel)
+        return False
+
+    if Var.counter == 6:
+        feed_info = client.get_feed()
+        data_dump_client(client, feed_info, 'feed')
+        data_dump_client(client, channel_dict, "channel_dict", channel)
+
+    Var.counter += 1
+
+    return
+
+
+def automation(client, channel, task=None, announcement=None, interval=3600):
+    join_dict = client.join_channel(channel)
+    data_dump(join_dict, 'join', channel)
+    client.active_ping(channel)
+    Var.counter = 0
+
+    channel_dict = get_channel_status(client, channel)
+    data_dump(channel_dict, 'channel_dict', channel)
+
+    if task == "mod":
+        channel_dict = get_channel_status(client, channel)
+        request_speaker_permission(client, channel, channel_dict, join_dict, mod=True)
+        Var.mod_channel_thread = mod_client(client, channel)
+        Var.ping_keep_alive_thread = ping_keep_alive_client(client, channel)
+
+    elif task == "music":
+        request_speaker_permission(client, channel, channel_dict, join_dict, music=True)
+        Var.music_thread = music_client(client, channel)
+        Var.ping_keep_alive_thread = ping_keep_alive_client(client, channel)
+
+    elif task == "track":
+        Var.track_thread = track_room_client(client, channel)
+
+    elif task == "welcome":
+        Var.welcome_thread = welcome_all_client(client, channel)
+        Var.ping_keep_alive_thread = ping_keep_alive_client(client, channel)
+
     if announcement:
-        send_room_chat(client, channel, announcement)
-        announce_thread = set_announcement(client, channel, announcement, interval)
+        # send_room_chat(client, channel, announcement)
+        Var.announce_thread = set_announcement(client, channel, announcement, interval)
 
-    elif channel_dict['channel_info']['is_private']:
-        message_1 = 'The share url for this room is'
-        message_2 = 'https://www.clubhouse.com/room/' + channel
+    elif channel_dict["channel_info"]["is_private"]:
+        message_1 = "The share url for this room is"
+        message_2 = f"https://www.clubhouse.com/room/{channel}"
         announcement = [message_1, message_2]
 
         send_room_chat(client, channel, announcement)
-        announce_thread = set_announcement(client, channel, announcement, interval)
+        Var.announce_thread = set_announcement(client, channel, announcement, interval)
 
-    else:
-        if announce_thread:
-            announce_thread.set()
-
-    global listen_ping_thread
-    if listen_ping_thread:
-        listen_ping_thread.set()
+    if Var.listen_ping_thread:
+        Var.listen_ping_thread.set()
 
     return join_dict
 
@@ -484,7 +603,7 @@ def listen_channel_ping(client):
     # Is this redundant with active mod?
     # Check for ping.
 
-    if active_mod:
+    if Var.active_mod:
         logging.info("moderation_tools.listen_channel_ping Response: active_mod is True")
         return False
 
@@ -504,12 +623,12 @@ def listen_channel_ping(client):
                 time_diff = time_now - time_created
                 time_diff = time_diff.total_seconds()
 
-                if time_diff < 30 and _user_id in approved_ping_list:
+                if time_diff < 30 and _user_id in Var.approved_ping_list:
                     respond = True
                     logging.info(f"moderation_tools.listen_channel_ping {_channel} {_user_name} {_message}")
 
                 if respond:
-                    active_mod_channel(client, _channel)
+                    automation(client, _channel, "mod",)
                     logging.info("moderation_tools.listen_channel_ping Triggered active_mod_channel")
                     return False
 
