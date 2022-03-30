3903c6e78801809f07a51fdf6a17c5c0594fba8c
deoncarlette <63321092+deoncarlette@users.noreply.github.com>
2022-03-30 00:33:54 -0500

    Update __init__.py

--- automod/__init__.py
+++ automod/__init__.py
@@ -1,7 +1,8 @@
 from . import logger
+# from .clubhouse import Clubhouse
 # from .moderator import ModClient as Mod
 # from .chat import ChatClient as Chat
 # from .audio import AudioClient as Audio
 # from .main import AutoModClient
 from .cli import AutoMod
-# from .clubhouse import Clubhouse
+
