a46af231197adda1286d9f90e712f1e5c0e11a0b
deoncarlette <63321092+deoncarlette@users.noreply.github.com>
2022-04-04 08:32:11 -0500

    Update automod.py

--- automod/automod.py
+++ automod/automod.py
@@ -12,3 +12,36 @@ class AutoModClient(Mod, Chat, Audio):
     def __init__(self):
         super().__init__()
 
+
+    def run_automod(self, channel, api_retry_interval_sec=10, thread_timeout=120,
+                    announcement=None, announcement_interval_min=60, announcement_delay=None):
+
+        # Start ChatClient
+        # Start TrackerClient
+        # Add Timestamp Announcement
+
+        join_info, channel_info, users_info, client_info = \
+            self.channel_init(channel, api_retry_interval_sec, thread_timeout,
+                              announcement, announcement_interval_min, announcement_delay)
+
+        self.active_mod_thread = self.init_active_channel(channel)
+
+        if self.get_chat_enabled(channel_info):
+            self.chat_client_thread = self.init_chat_client(channel)
+
+
+    @set_interval(30)
+    def init_chat_client(self, channel, response_interval=120, delay=10):
+        self.run_chat_client(channel, response_interval, delay)
+        return True
+
+    @set_interval(15)
+    def init_active_channel(self, channel, message_delay=2):
+        self.active_channel(channel, message_delay)
+        return True
+
+
+
+    waiting_ping_thread = None
+    active_mod_thread = None
+    chat_client_thread = None
