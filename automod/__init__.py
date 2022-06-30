695454dc293c0b34df08f03a9c712bff97568316
Deandra Whitby <63321092+deoncarlette@users.noreply.github.com>
2022-06-29 20:50:14 -0500

    Delete __init__.py

--- automod/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import logger
-from .tracker import Tracker
-from .chat import ChatClient as Chat
-from .audio import AudioClient as Audio
-from .moderator import ModClient as Mod
-from .cli import AutoMod
