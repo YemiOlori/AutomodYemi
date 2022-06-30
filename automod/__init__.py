856baa8d3483fbe57b0765017f7424b2bd5a6f8e
Deandra Whitby <63321092+deoncarlette@users.noreply.github.com>
2022-06-29 20:47:10 -0500

    Update __init__.py

--- automod/__init__.py
+++ automod/__init__.py
@@ -1,6 +1,8 @@
-from . import logger
+from .logger import Logger
 from .tracker import Tracker
 from .chat import ChatClient as Chat
 from .audio import AudioClient as Audio
 from .moderator import ModClient as Mod
 from .cli import AutoMod
+
+
